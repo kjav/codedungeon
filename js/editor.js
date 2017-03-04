6d85fe9cc21c174674343d624d727590a888d150
Sam <sam.rr.lee@gmail.com>
2017-03-04 01:16:49 +0100

    Code editor executes globally

--- js/editor.js
+++ js/editor.js
@@ -1,3 +1,19 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
+
+function run_code() {
+  console.log(window.eval(editor.getValue()));
+}
+
+// Add ctrl-enter keybinding for running code
+editor.commands.addCommand({
+  name: 'myCommand',
+  bindKey: { win: "Ctrl-Enter", mac: "Command-Enter" },
+  exec: function(editor) {
+    run_code();
+  },
+  readOnly: true
+});
+
+$("#run").click(run_code);
