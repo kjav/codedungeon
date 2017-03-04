bf0b88e8a8b0c8b7d1677ec6cf31e1c28b826334
dnadan2 <_dan_@live.com>
2017-03-04 16:21:08 +0000

    added male sprite

--- js/drawing/sprites.js
+++ js/drawing/sprites.js
@@ -1,4 +1,4 @@
-var data = {
+var girlData = {
     images: ["http://i.imgur.com/Cy7KO7a.png"],
     frames: {width:48, height:48, count:16, regX: 0, regY:0, spacing:0, margin:0},
     animations: {
@@ -29,5 +29,38 @@ var data = {
     },
     framerate: 31
 };
-var spriteSheet = new createjs.SpriteSheet(data);
-var animation = new createjs.Sprite(spriteSheet, "stopLeft");
+
+var boyData = {
+    images: ["http://i.imgur.com/9bb4qHJ.png"],
+    frames: {width:48, height:48, count:16, regX: 0, regY:0, spacing:0, margin:0},
+    animations: {
+        walkLeft: {
+            frames: [1,5,9,13]
+        },
+        stopLeft: {
+            frames: [1]
+        },
+        walkRight: {
+            frames: [3,7,11,15]
+        },
+        stopRight: {
+            frames: [3]
+        },
+        walkUp: {
+            frames: [2,6,10,14]
+        },
+        stopUp: {
+            frames: [2]
+        },
+        walkDown: {
+            frames: [0,4,8,12]
+        },
+        stopDown: {
+            frames: [0]
+        }
+    },
+    framerate: 31
+};
+var girlSpriteSheet = new createjs.SpriteSheet(girlData);
+var boySpriteSheet = new createjs.SpriteSheet(boyData);
+var animation = new createjs.Sprite(boySpriteSheet, "stopLeft");
