2e5b08d5260883644c12c98bfebce12880c0f6a7
dnadan2 <_dan_@live.com>
2017-03-04 18:25:14 +0000

    fixed scaling border on wall texture + improved quality

--- js/drawing/image.js
+++ js/drawing/image.js
@@ -1,15 +1,15 @@
 var imageLoader;
 
 var manifest = [
-    { src: "http://i.imgur.com/15ErwZW.png", id: "stone" },
+    { src: "http://i.imgur.com/ZFw0Tkg.png", id: "stone" },
     { src: "http://i.imgur.com/sGowEpM.jpg", id: "grass" },
-    { src: "http://i.imgur.com/32TLJMe.png", id: "wallleft" },
-    { src: "http://i.imgur.com/0BIh4ee.png", id: "wallright" },
-    { src: "http://i.imgur.com/EKLgRE3.png", id: "walltop" },
-    { src: "http://i.imgur.com/0J3iQbk.png", id: "wallbottom" },
-    { src: "http://i.imgur.com/AolMN0i.png", id: "walltopleft" },
-    { src: "http://i.imgur.com/PnYDmkG.png", id: "walltopright" },
-    { src: "http://i.imgur.com/nT77LtZ.png", id: "wallbottomleft" },
-    { src: "http://i.imgur.com/0IUhgFo.png", id: "wallbottomright" },
+    { src: "http://i.imgur.com/RpfF7sM.png", id: "wallleft" },
+    { src: "http://i.imgur.com/dvRyFhs.png", id: "wallright" },
+    { src: "http://i.imgur.com/hGXMxqQ.png", id: "walltop" },
+    { src: "http://i.imgur.com/igXKEsZ.png", id: "wallbottom" },
+    { src: "http://i.imgur.com/B2GpLK2.png", id: "walltopleft" },
+    { src: "http://i.imgur.com/fEcUkgB.png", id: "walltopright" },
+    { src: "http://i.imgur.com/S0ZZm1Z.png", id: "wallbottomleft" },
+    { src: "http://i.imgur.com/gOYZx5N.png", id: "wallbottomright" },
     { src: "http://i.imgur.com/qPr1A4l.png", id: "girlfront" }
 ];
