db981c520225b8f7692e470f1a8c0f4c0bfbb2aa
dnadan2 <_dan_@live.com>
2017-03-04 17:41:24 +0000

    removed blackspace from walls and new block texture

--- js/drawing/image.js
+++ js/drawing/image.js
@@ -1,15 +1,15 @@
 var imageLoader;
 
 var manifest = [
-    { src: "http://i.imgur.com/c05eK8H.png", id: "stone" },
+    { src: "http://i.imgur.com/15ErwZW.png", id: "stone" },
     { src: "http://i.imgur.com/sGowEpM.jpg", id: "grass" },
-    { src: "http://i.imgur.com/J7Nu1qA.png", id: "wallleft" },
-    { src: "http://i.imgur.com/5MenRvj.png", id: "wallright" },
-    { src: "http://i.imgur.com/IbzT8H9.png", id: "walltop" },
-    { src: "http://i.imgur.com/AdOTDZv.png", id: "wallbottom" },
-    { src: "http://i.imgur.com/dm8gm0l.png", id: "walltopleft" },
-    { src: "http://i.imgur.com/jjsdNkc.png", id: "walltopright" },
-    { src: "http://i.imgur.com/chp4BDL.png", id: "wallbottomleft" },
-    { src: "http://i.imgur.com/HZYuTED.png", id: "wallbottomright" },
+    { src: "http://i.imgur.com/32TLJMe.png", id: "wallleft" },
+    { src: "http://i.imgur.com/oTJx2tZ.png", id: "wallright" },
+    { src: "http://i.imgur.com/EKLgRE3.png", id: "walltop" },
+    { src: "http://i.imgur.com/0J3iQbk.png", id: "wallbottom" },
+    { src: "http://i.imgur.com/AolMN0i.png", id: "walltopleft" },
+    { src: "http://i.imgur.com/PnYDmkG.png", id: "walltopright" },
+    { src: "http://i.imgur.com/nT77LtZ.png", id: "wallbottomleft" },
+    { src: "http://i.imgur.com/0IUhgFo.png", id: "wallbottomright" },
     { src: "http://i.imgur.com/qPr1A4l.png", id: "girlfront" }
 ];
