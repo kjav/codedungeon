1fb35939d3324869ea215acee22d5270928e6b3e
Sam <sam.rr.lee@gmail.com>
2017-03-04 22:00:21 +0100

    added lava animation

---
+++ js/drawing/LavaGraphic.js
@@ -0,0 +1,28 @@
+function LavaGraphic(spriteSheet, startAnimation) {
+    var shape = new createjs.Sprite(spriteSheet, startAnimation);
+    shape.x = 0;
+    shape.y = 0;
+    shape.gridX = 0;
+    shape.gridY = 0;
+    shape.setGridPos = function(i, j) {
+      shape.setGridX(i);
+      shape.setGridY(j);
+    };
+    shape.setGridX = function(i) {
+      shape.gridX = i;
+      shape.x = i * gridSize;
+    };
+    shape.setGridY = function(j) {
+      shape.gridY = j;
+      shape.y = j * gridSize;
+    };
+
+    shape.resize = function() {
+      shape.scaleX = gridSize / 128;
+      shape.scaleY = gridSize / 128;
+    };
+
+    shape.resize();
+
+    return shape;
+}
