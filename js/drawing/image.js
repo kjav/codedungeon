12ed487428d311cc62f3a85471a6e8c6d4de191e
dnadan2 <_dan_@live.com>
2017-03-04 17:57:21 +0000

    fixed blemish on texture

--- js/drawing/image.js
+++ js/drawing/image.js
@@ -4,7 +4,7 @@ var manifest = [
     { src: "http://i.imgur.com/15ErwZW.png", id: "stone" },
     { src: "http://i.imgur.com/sGowEpM.jpg", id: "grass" },
     { src: "http://i.imgur.com/32TLJMe.png", id: "wallleft" },
-    { src: "http://i.imgur.com/oTJx2tZ.png", id: "wallright" },
+    { src: "http://i.imgur.com/0BIh4ee.png", id: "wallright" },
     { src: "http://i.imgur.com/EKLgRE3.png", id: "walltop" },
     { src: "http://i.imgur.com/0J3iQbk.png", id: "wallbottom" },
     { src: "http://i.imgur.com/AolMN0i.png", id: "walltopleft" },
