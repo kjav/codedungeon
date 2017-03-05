5a0374e3df4da88fe58fc01ba210f6423331dbbc
dnadan2 <_dan_@live.com>
2017-03-05 02:45:52 +0000

    designed 3rd level

--- js/drawing/LevelsDictionary.js
+++ js/drawing/LevelsDictionary.js
@@ -66,5 +66,37 @@ var levels = {
             console.log('GoalCoord x:' + state['GoalCoords'].x + ', GoalCoords y: ' + state['GoalCoords'].y);
             return state['GoalCoords'].x == playerGraphic.gridX && state['GoalCoords'].y == playerGraphic.gridY && ((itemsInLevel.length > 0) ? itemsInLevel[0].taken : true);
         }
+    },
+
+    LevelThree: {
+        map: [
+            [6, 4, 4, 4, 4, 4, 4, 4, 4, 7],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+            [2, 1, 1, 1, 1, 0, 1, 1, 1, 3],
+            [2, 1, 1, 1, 1, 0, 1, 1, 1, 3],
+            [8, 5, 5, 5, 5, 5, 5, 5, 5, 9]
+        ],
+        startingText: "TEST LEVEL 2 TEXT.",
+        endingText: "u gon die now.",
+        state: {
+            GoalCoords: {
+                x: 0,
+                y: 0
+            },
+           startCoords: {
+               x: 5,
+               y: 8
+           },
+           startOrientation: FACING_NORTH
+        },
+        checkGoal: function(state) {
+            return bossBeaten; // change to win settings
+        }
     }
+
 };
